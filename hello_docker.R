f7eb7aa1d2cbf67a1d34f6bac881e05f4d9b8c6c
Your GitHub Username <liem20k@gmail.com>
2025-06-08 12:31:57 +0700

    thêm R

---
+++ hello_docker.R
@@ -0,0 +1,8 @@
+# hello_docker.R 🐳
+# File minh họa chạy R trong dự án Docker
+
+cat("Xin chào từ R trong Docker! 🎉\n")
+
+# Tính tổng từ 1 đến 100
+tong <- sum(1:100)
+cat("Tổng từ 1 đến 100 là:", tong, "\n")
